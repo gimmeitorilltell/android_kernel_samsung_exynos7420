6b19d9ec85343ec682fc4eb7ef0ddb6133d77d69
Lukas0610 <mail@lukasberger.at>
2018-02-04 18:44:31 +0100

    blkdev: adjust default queue-flags
    Disables I/O-stats and data collection for random-driver
    
    Change-Id: I40df5721e747a3e9ee40fc0a406b40c84eb1ec31

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -477,10 +477,10 @@ struct request_queue {
 #define QUEUE_FLAG_JOURNAL_TAG     31      /* supports JOURNAL_DATA_TAG */
 #endif
 
-#define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_IO_STAT) |		\
+#define QUEUE_FLAG_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
 				 (1 << QUEUE_FLAG_STACKABLE)	|	\
 				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_ADD_RANDOM))
+				 (0 << QUEUE_FLAG_ADD_RANDOM))
 
 static inline void queue_lockdep_assert_held(struct request_queue *q)
 {
